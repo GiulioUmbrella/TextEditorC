11322471ba83c718fae2ab55a1a27254e6ade112
GiulioUmbrella <giulio.umbrella@gmail.com>
2025-08-24 21:20:02 +0200

    Disable raw mode
    Disable the echo flag in the terminal setup

--- src/main.c
+++ src/main.c
@@ -1,3 +1,13 @@
+#include "terminal.h"
+
+#include <unistd.h>
+
+
 int main() {
+  enableRawMode();
+  
+  char c;
+  while (read(STDIN_FILENO, &c,1) == 1 && c != 'q' );
+  
   return 0;
 }
---
+++ src/terminal.c
@@ -0,0 +1,16 @@
+#include "terminal.h"
+
+#include <termios.h>
+#include <unistd.h>
+
+
+
+void enableRawMode() {
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
