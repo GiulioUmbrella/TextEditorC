83c2df85d5b9e1850520e3e517a1b97b97d9af54
GiulioUmbrella <giulio.umbrella@gmail.com>
2025-08-27 12:07:59 +0200

    Add default char to print
    If timeout triggers the character evaluated in '\0'

--- src/main.c
+++ src/main.c
@@ -24,7 +24,18 @@ int main() {
 
 #else
 int main() {
-  return 42;
+  enableRawMode();
+
+	while (1) {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
+        if (iscntrl(c)) {
+        printf("%d\r\n", c);
+        } else {
+        printf("%d ('%c')\r\n", c, c);
+        }
+    if (c == 'q') break;
+  }
 }
 
 #endif
